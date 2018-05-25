aa56cb53d8b5337bc72f126bd0ad5b238c9aa1a8
jpc@wxyz.io Courtney <jpc@wxyz.io>
2018-05-25 04:01:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
-  else if (string.toLowerCase())
-     return "YES INDEED!";
-   if (string.toUpperCase()) 
+  if (string.toLowerCase())
   return "I can't hear you!";
- 
+  else if (string.toUpperCase()) 
+    return "YES INDEED!";
   else if (string == "I love you, Grandma.")
     return "I love you, too."
     
